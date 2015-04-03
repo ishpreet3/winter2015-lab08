da737ea498a9bb896c060010be2f9d410785173e
Ishpreet Rattan <ishpreet_rattan@hotmail.com>
2015-04-03 16:55:20 -0700

    done up to enabling sessions Signed-off-by:Ishpreet Rattan <ishpreet_rattan@hotmail.com>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -110,7 +110,7 @@ $autoload['language'] = array();
 */
 
 $autoload['model'] = array();
-
+$autoload['libraries'] = array('parser','database', 'session');
 
 /* End of file autoload.php */
 /* Location: ./application/config/autoload.php */
