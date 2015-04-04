fb72abd363d21814e4a69cabf07f75c75e026006
Ishpreet Rattan <ishpreet_rattan@hotmail.com>
2015-04-03 17:28:36 -0700

    finished lab Signed-off-by:Ishpreet Rattan <ishpreet_rattan@hotmail.com>

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,30 +1,36 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Our public content. 
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
  */
-
 class Auth extends Application {
-  function __construct() {
-    parent::__construct();
-    $this->load->helper('url');
-  }
-  function index() {
-    $this->data['pagebody'] = 'login';
-    $this->render();
-  }
-  
-    function submit() {
-    $key = $_POST['userid'];
-      $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
-    $user = $this->users->get($key);
-    if ($password == (string) $user->password) {
-      $this->session->set_userdata('userID',$key);
-      $this->session->set_userdata('userName',$user->name);
-      $this->session->set_userdata('userRole',$user->role);
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
     }
-    redirect('/');
-}
+    function index()
+    {
+        $this->data['pagebody'] = 'login';
+        $this->render();
+    }
+     function login() {
+        $key = $_POST['userid'];
+        $user = $this->users->get($key);
+        
+        if (password_verify($this->input->post('password'), $user->password)) {
+          $this->session->set_userdata('userID',$key);
+          $this->session->set_userdata('userName',$user->name);
+          $this->session->set_userdata('userRole',$user->role);
+        }
+        
+        redirect('/');
+    }
+    
+    function logout() {
+        $this->session->sess_destroy();
+        redirect('/');
+    }
 }
